9991e477a9d19f28e46b0877e9b0ce355080c671
Chiouder <s4068959@student.rmit.edu.au>
2025-03-19 14:41:34 +1100

    Initial commit with completed Task1(Created and populated 'login' and 'music' table in Dynamodb)

---
+++ main.py
@@ -0,0 +1,67 @@
+from dynamo_manager import DynamoDBManager  # Import only the class
+from s3_manager import S3Manager
+
+def main():
+    db = DynamoDBManager()
+    json_file = '2025a1.json'
+    login_table_schema = {
+        "KeySchema": [{"AttributeName": "email", "KeyType": "HASH"}],
+        "AttributeDefinitions": [{"AttributeName": "email", "AttributeType": "S"}],
+        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+    }
+
+    music_table_schema = {
+        "KeySchema": [{"AttributeName": "title", "KeyType": "HASH"}, # Partition key
+                      {"AttributeName": "album", "KeyType": "RANGE"} # Sort key
+                      ],
+        "AttributeDefinitions":[
+            {"AttributeName": "title", "AttributeType": "S"},
+            # {"AttributeName": "artist", "AttributeType": "S"},
+            # {"AttributeName": "year", "AttributeType": "N"},
+            {"AttributeName": "album", "AttributeType": "S"},
+            # {"AttributeName": "image_url", "AttributeType": "S"}
+        ],
+        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+    }
+
+    # # TASK 1.1 -- Create a 'login' table and insert data
+    # db.create_table('login', login_table_schema)
+    #
+    # # Insert data
+    # for i in range(10):
+    #     email = f"s4068959{i}@student.rmit.edu.au"
+    #     user_name = f"JordanChiou{i}"
+    #     password = "".join(str((i+j) % 10) for j in range(6))
+    #
+    #     # data dict
+    #     user_data = {
+    #         'email': email,
+    #         'user_name': user_name,
+    #         'password': password
+    #     }
+    #
+    #     # insert data into the table
+    #     db.insert_data('login', user_data)
+    #
+    # # TASK 1.2 -- create a table titled 'music'
+    # db.create_table('music', music_table_schema)
+    #
+    # # TASK1.3 -- Load data from json file
+    # db.load_data_from_json_into_table('music', json_file, 'title', 'album')
+
+    # # TASK 2 -- Download from img_url and upload images to S3
+    # s3_manager = S3Manager()
+    # bucket_name = 'media-storage-s4068959'
+    # # Create a bucket
+    # s3_manager.create_s3_bucket(bucket_name)
+    # # Download and Upload images to S3
+    # s3_manager.download_and_upload(json_file, bucket_name)
+
+
+if __name__ == "__main__":
+    main()  # Run the script
+
+
+# Reference:
+# S3 creating and upload file: https://docs.aws.amazon.com/code-library/latest/ug/python_3_s3_code_examples.html
+# requests package: https://www.simplilearn.com/tutorials/python-tutorial/python-requests
---
+++ s3_manager.py
@@ -0,0 +1,129 @@
+# Third-party dependencies (Need to install via `pip install requests`)
+import requests
+
+import os
+import boto3
+import json
+
+
+class S3Manager:
+    def __init__(self, region='us-east-1'):
+        """
+       Initializes the S3 client.
+
+       :param str region: AWS region where the S3 bucket is created.
+       """
+        self.s3_client = boto3.client('s3', region_name=region) # Create an S3 client
+        self.region = region
+
+    def create_s3_bucket(self, bucket_name:str) -> None:
+        """
+        Creates an S3 bucket with the specified name and region.
+        :param bucket_name: The name of the S3 bucket to create.
+        :return: None
+        """
+        try:
+            if self.region == 'us-east-1':
+                self.s3_client.create_bucket(Bucket=bucket_name)
+            else: # For regions other than us-east-1, specify the location constraint, AWS require that.
+                self.s3_client.create_bucket(
+                    Bucket= bucket_name,
+                    CreateBucketConfiguration={'LocationConstraint': self.region}
+                    )
+            print(f"✅ S3 bucket '{bucket_name}' created successfully in region {self.region}!")
+        except self.s3_client.exceptions.BucketAlreadyOwnedByYou:
+            print(f"⚠️ S3 bucket '{bucket_name}' already exists and is owned by you.")
+        except self.s3_client.exceptions.BucketAlreadyExists:
+            print(f"❌ S3 bucket '{bucket_name}' already exists and is owned by someone else. Choose a different name.")
+        except Exception as e:
+            print(f"❌ Failed to create bucket: {e}")
+
+    def upload_file_to_bucket(self, file_path: str, bucket_name: str, s3_key: str) -> None:
+        """
+        Uploads a file to the specified S3 bucket.
+
+        :param str file_path: Path to the local file.
+        :param str bucket_name: S3 bucket where the file will be uploaded.
+        :param str s3_key: The S3 object key (file path in S3).
+        :return: None
+        """
+        try:
+            self.s3_client.upload_file(file_path, bucket_name, s3_key)
+            print(f"📤 Uploaded {file_path} to S3: s3://{bucket_name}/{s3_key}")
+        except Exception as e:
+            print(f"❌ Failed to upload {file_path} to S3: {e}")
+
+    def download_and_upload(self, json_file: str, bucket_name:str, local_folder: str = 'images') -> None:
+        """
+      Downloads artist images from the given JSON file and uploads them to an S3 bucket.
+
+      :param str json_file: Path to the JSON file containing 'image_url' values.
+      :param str bucket_name: Name of the S3 bucket to upload images to.
+      :param str local_folder: Local directory to store downloaded images before upload.
+      :return: None
+      """
+        try:
+            with open(json_file, 'r', encoding="utf-8") as file:
+                loaded_data = json.load(file) # Load the JSON file
+
+            # Check if the JSON file contains a list of songs
+            if 'songs' not in loaded_data or not isinstance(loaded_data['songs'], list):
+                raise ValueError(f"❌ Invalid JSON format: Missing 'songs' key or it's not a list.")
+
+            songs = loaded_data['songs']
+
+            # Ensure the local folder exists
+            if not os.path.exists(local_folder):
+                os.makedirs(local_folder) # Create the local folder 'images' by default if it doesn't exist
+
+            for each_song in songs:
+                img_url = each_song.get("img_url", None) # if not exist return None
+                artist = each_song.get("artist", "unknown").replace(" ", "_") # Replace spaces with underscores to clean the artist name
+
+                if not img_url:
+                    print(f"⚠️ Skipping {artist}: No image URL found.")
+                    continue # Skip if no image URL is found
+
+                # Determine file name to keep them in uniform format
+                file_name = f"{artist}.jpg"
+                file_path = os.path.join(local_folder, file_name)  # Local file path/artist.jpg
+
+                # Check if the file already exists locally, reduce the download time. So we can delete s3 after end lab to reduce cost
+                if os.path.exists(file_path):
+                    print(f"⚠️ Skipping download: {file_name} already exists locally.")
+                else:
+
+                    try:
+                        #Download the image
+                        print(f"⬇️ Downloading {file_name} from {img_url}...")
+                        response = requests.get(img_url, stream = True, timeout = 10) #Stream the response content, avoid loading the entire file into memory
+                        response.raise_for_status()  # Raise error for bad response
+
+                        with open(file_path, "wb") as img_file: # Open the file in binary write mode (good for img or video)
+                            for chunk in response.iter_content(4096): # Write the content in 4KB chunks
+                                img_file.write(chunk) # Write the chunk to the file directly, so won't occupy memory
+
+                        print(f"✅ Successfully downloaded: {file_name}")
+
+                    except requests.RequestException as e:
+                        print(f"❌ Failed to download {img_url}: {e}")
+                        continue
+
+                # Upload the image to S3
+                try:
+                    ## Change the prefix here to categorize the images in S3
+                    s3_key = f"artist-images/{file_name}" # S3 key: artist-images/artist.jpg
+
+                    self.upload_file_to_bucket(file_path, bucket_name, s3_key)
+                except Exception as e:
+                    print(f"❌ Failed to upload {file_name} to S3: {e}")
+
+        except Exception as e:
+            print(f"❌ Error processing JSON: {e}")
+
+
+
+
+
+
+
