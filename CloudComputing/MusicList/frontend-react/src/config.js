0ecb0b5efe1d34c49b41db699a48717e0a7daa2c
jordan0726 <jordan89726@gmail.com>
2025-04-07 00:19:16 +1000

    Update requirement.txt
    Add python-jose

--- CloudComputing/MusicList/frontend-react/src/config.js
+++ CloudComputing/MusicList/frontend-react/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-  backendBaseURL: "http://ec2-18-215-182-128.compute-1.amazonaws.com"
+  backendBaseURL: "http://ec2-54-159-16-90.compute-1.amazonaws.com"
 };
 
 export default config;
