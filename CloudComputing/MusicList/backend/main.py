b4790f5efc77ec58472f44068383d462d1bcf088
jordan0726 <jordan89726@gmail.com>
2025-04-07 19:50:00 +1000

    Cookie Test

--- CloudComputing/MusicList/backend/main.py
+++ CloudComputing/MusicList/backend/main.py
@@ -5,8 +5,8 @@ from backend.api import auth
 app = FastAPI()
 
 origins = [
-    "http://ec2-54-165-19-130.compute-1.amazonaws.com",  # e.g. http://ec2-xx-xxx-xxx-xxx.compute-1.amazonaws.com
-    "http://54.165.19.130",        # Frontend IP
+    "http://ec2-13-218-56-9.compute-1.amazonaws.com",  # e.g. http://ec2-xx-xxx-xxx-xxx.compute-1.amazonaws.com
+    "http://13.218.56.9",        # Frontend IP
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
--- CloudComputing/MusicList/frontend-react/src/config.js
+++ CloudComputing/MusicList/frontend-react/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-  backendBaseURL: "http://ec2-54-152-159-218.compute-1.amazonaws.com"
+  backendBaseURL: "http://ec2-54-160-169-238.compute-1.amazonaws.com"
 };
 
 export default config;
