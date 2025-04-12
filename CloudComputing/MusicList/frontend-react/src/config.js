f4a513b433db7b803ddca2ea172d14a240248208
jordan0726 <jordan89726@gmail.com>
2025-04-12 12:39:24 +1000

    Update react build

--- CloudComputing/MusicList/frontend-react/src/config.js
+++ CloudComputing/MusicList/frontend-react/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-  backendBaseURL: "http://ec2-44-210-126-150.compute-1.amazonaws.com",
+  backendBaseURL: "http://ec2-18-233-150-168.compute-1.amazonaws.com",
   apiGatewayURL: "https://z3y9r4o7p9.execute-api.us-east-1.amazonaws.com/prod",
 };
 
